5e74a2d61ca6ecdd65f971262ab370ae4616b52e
KiriukhinD <dm.arz2018@yandex.ru>
2022-12-08 17:44:20 +0300

    TelegramBot

---
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -0,0 +1,42 @@
+package pro.sky.telegrambot.listener;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Service
+public class TelegramBotUpdatesListener implements UpdatesListener {
+
+    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+
+    @Autowired
+    private TelegramBot telegramBot;
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+
+        updates.forEach(update -> {
+            logger.info("Processing update: {}", update);
+            long chatId = update.message().chat().id();
+            System.out.println("chat id [" + update.message().chat().id() + "]");
+            SendResponse response = telegramBot.execute(new SendMessage(chatId, "Hello!"));
+            SendResponse response1 = telegramBot.execute(new SendMessage(chatId, "garold!"));
+        });
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+}
